aeee94606eae5c7146ad9b1d06e27aed060bbbfc
msusz <78818145+msusz@users.noreply.github.com>
2021-04-13 17:43:02 +0200

    translate

---
+++ varnisher.h
@@ -0,0 +1,12 @@
+#ifndef SUSZCZYKMARCELINA_VARNISHER_H
+#define SUSZCZYKMARCELINA_VARNISHER_H
+#include "pojazd.h"
+
+class Varnisher {
+public:
+
+void varnish(Vehicle* vehicle, const string &newColor);
+};
+
+
+#endif //SUSZCZYKMARCELINA_VARNISHER_H
